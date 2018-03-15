31f6b2b4638a0823c078fc170d5f1850ff1a0364
Martin Lindström <martin.lindstrom@litsec.se>
2018-03-15 19:18:03 +0100

    END-202 Fixes for local docker execution

--- idp/src/main/scripts/runDockerDev.sh
+++ idp/src/main/scripts/runDockerDev.sh
@@ -14,13 +14,13 @@ docker run -d --name eidas-connector --restart=always \
   -e IDP_DEVEL_MODE=true \
   -e IDP_SERVER_HOSTNAME=localhost -e IDP_SERVER_PORT=9200 \
   -e IDP_ENTITY_ID="https://eunode.eidastest.se/idp2" \
-  -e SP_ENTITY_ID="https://eunode.eidastest.se/connector-sp" \
+  -e SP_ENTITY_ID="https://localhost:9200/idp/metadata/sp" \
   -e FEDERATION_METADATA_URL="https://qa.md.swedenconnect.se/entities" \
-  -e FEDERATION_METADATA_VALIDATION_CERT=/tmp/eidas-connector-credentials/metadata/sc-qa-metadata-validation-cert.crt \
+  -e FEDERATION_METADATA_VALIDATION_CERT=/etc/eidas-connector-credentials/metadata/sc-qa-metadata-validation-cert.crt \
   -e SECONDARY_FEDERATION_METADATA_URL="https://eid.svelegtest.se/metadata/feed" \
-  -e SECONDARY_FEDERATION_METADATA_VALIDATION_CERT=/tmp/eidas-connector-credentials/metadata/sveleg-metadata-validation-cert.crt \  
-  -e EIDAS_METADATA_SERVICE_LIST_URL=file:///etc/eidas-connector/mock/metadataList.xml \
-  -e EIDAS_METADATA_URL=file:///etc/eidas-connector/mock/metadata.xml \
+  -e SECONDARY_FEDERATION_METADATA_VALIDATION_CERT=/etc/eidas-connector-credentials/metadata/sveleg-metadata-validation-cert.crt \
+  -e EIDAS_METADATA_SERVICE_LIST_URL=file:///etc/eidas-connector/metadata/metadataList.xml \
+  -e EIDAS_METADATA_URL=file:///etc/eidas-connector/metadata/metadata.xml \
   -e EIDAS_METADATA_IGNORE_SIGNATURE_VALIDATION=true \
   -e IDP_LOG_SETTINGS_FILE=/opt/eidas-connector/shibboleth/conf/logback-devel.xml \
   -e IDP_LOG_CONSOLE=true \
@@ -35,7 +35,9 @@ docker run -d --name eidas-connector --restart=always \
   -e TOMCAT_PROXY_SHARED_SECRET=123456 \
   -v /tmp/eidas-connector-credentials:/etc/eidas-connector-credentials \
   -v /tmp/eidas-connector:/var/log/eidas-connector \
-  -v /private/etc/eidas-connector:/etc/eidas-connector \
+  -v ${EIDAS_LOCAL_ENV}:/etc/eidas-connector \
   docker.eidastest.se:5000/eidas-connector-idp
     
 # docker logs -f eidas-connector
+# -v /private/etc/eidas-connector:/etc/eidas-connector \
+
