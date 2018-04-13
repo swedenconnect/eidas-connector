ac9c91c1059bb66405b6c2e7a3994e3534f84a4c
Martin Lindström <martin.lindstrom@litsec.se>
2018-04-13 23:33:52 +0200

    [END-178] Refuses representative attributes
    The eIDAS connector now refuses to accept an assertion that contains
    eIDAS representative attributes

--- sp/src/main/java/se/elegnamnden/eidas/idp/connector/sp/validation/EidasAttributeStatementValidator.java
+++ sp/src/main/java/se/elegnamnden/eidas/idp/connector/sp/validation/EidasAttributeStatementValidator.java
@@ -19,12 +19,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.chrono.ISOChronology;
+import org.opensaml.saml.common.assertion.AssertionValidationException;
 import org.opensaml.saml.common.assertion.ValidationContext;
+import org.opensaml.saml.common.assertion.ValidationResult;
+import org.opensaml.saml.saml2.core.Assertion;
+import org.opensaml.saml.saml2.core.Attribute;
+import org.opensaml.saml.saml2.core.AttributeStatement;
 import org.opensaml.saml.saml2.core.AuthnRequest;
 import org.opensaml.saml.saml2.core.Extensions;
+import org.opensaml.saml.saml2.core.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import se.litsec.eidas.opensaml.ext.RequestedAttribute;
 import se.litsec.eidas.opensaml.ext.RequestedAttributes;
+import se.litsec.eidas.opensaml.ext.attributes.AttributeConstants;
 import se.litsec.opensaml.common.validation.CoreValidatorParameters;
 import se.litsec.swedisheid.opensaml.saml2.validation.SwedishEidAttributeStatementValidator;
 
@@ -36,6 +47,48 @@ import se.litsec.swedisheid.opensaml.saml2.validation.SwedishEidAttributeStateme
  * @author Martin Lindström (martin.lindstrom@litsec.se)
  */
 public class EidasAttributeStatementValidator extends SwedishEidAttributeStatementValidator {
+  
+  /** Class logger. */
+  private final Logger log = LoggerFactory.getLogger(EidasAttributeStatementValidator.class);
+
+  /**
+   * Validates using the default functionality, but adds checks to ensure that we don't receive any representative
+   * attributes.
+   */
+  @Override
+  public ValidationResult validate(Statement statement, Assertion assertion, ValidationContext context)
+      throws AssertionValidationException {
+
+    ValidationResult result = super.validate(statement, assertion, context);
+    if (result == ValidationResult.INVALID) {
+      return result;
+    }
+    AttributeStatement attributeStatement = (AttributeStatement) statement;
+    
+    if (attributeStatement.getAttributes().stream().filter(a -> isRepresentativeAttribute(a)).findAny().isPresent()) {
+      final String msg = "Assertion contains eIDAS representative attributes - not supported";      
+      log.info("{} [assertion-id:{}]", msg, assertion.getID());
+      context.setValidationFailureMessage(msg);
+      return ValidationResult.INVALID;
+    }
+    
+    return result;
+  }
+
+  /**
+   * Predicate that tells if the supplied attribute is a representative attribute.
+   * 
+   * @param attribute
+   *          the attribute to check
+   * @return {@code true} if the attribute is a representative attribute and {@code false} otherwise
+   */
+  private static boolean isRepresentativeAttribute(Attribute attribute) {
+    if (attribute.getName() == null) {
+      return false;
+    }
+    return (attribute.getName().startsWith(AttributeConstants.REPRESENTATIVE_NATURAL_PERSON_PREFIX)
+        || attribute.getName().startsWith(AttributeConstants.REPRESENTATIVE_LEGAL_PERSON_PREFIX));
+  }
 
   /**
    * Extracts the {@link RequestedAttribute} objects from the {@code AuthnRequest}.
